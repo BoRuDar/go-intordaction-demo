593880eddd3fa0d4033c2377540019acaaf34f76
Bogdan <19273017+BoRuDar@users.noreply.github.com>
2020-10-08 22:56:40 +0200

    cli-2

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/BoRuDar/go-introduction-demo/internal/services"
 )
 
 func main() {
-	s := services.NewGitHub(http.DefaultClient)
-	res, err := s.GetStars("borudar", "configuration")
-	if err != nil {
-		log.Fatal("error: ", err)
-	}
+	cmd := flag.String("cmd", "stars", "Command to execute")
+	owner := flag.String("owner", "", "Owner of the repository")
+	repoName := flag.String("repo", "", "Name of the repository")
+	flag.Parse()
 
-	fmt.Printf("%+v", res)
+	svc := services.NewGitHub(http.DefaultClient)
+
+	switch strings.ToLower(*cmd) {
+
+	case "stars":
+		res, err := svc.GetStars(*owner, *repoName)
+		log.Printf("cmd: %svc | err: %v | result: %+v\n", *cmd, err, res)
+
+	default:
+		log.Fatalf("Command not found")
+	}
 }
